ca706b7dfd1395b96b8ed62ad77dd9d8064d1300
gohelvivek <vivekgohel93027@gmail.com>
2022-08-25 18:46:42 +0530

    FIRST

---
+++ app/src/main/java/com/example/makemytiffin/Login.java
@@ -0,0 +1,20 @@
+package com.example.makemytiffin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(Login.this,Register.class));
+    }
+}
